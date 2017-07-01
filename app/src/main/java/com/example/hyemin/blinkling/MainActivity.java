777e4114ba373f0e35b4ea9f5691e4a4ef394055
HyeminS <shm1182@hotmail.com>
2017-07-01 22:17:29 +0900

    스플래쉬 뷰까지 완료

--- app/src/main/java/com/example/hyemin/blinkling/MainActivity.java
+++ app/src/main/java/com/example/hyemin/blinkling/MainActivity.java
@@ -6,7 +6,9 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,12 +16,24 @@ public class MainActivity extends AppCompatActivity {
     private BottomNavigationView bottomNavigation;
     private Fragment fragment;
     private FragmentManager fragmentManager;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bottomNavigation = (BottomNavigationView)findViewById(R.id.bottom_navigation);
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+        // Get the ActionBar here to configure the way it behaves.
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true); //커스터마이징 하기 위해 필요
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼, 디폴트로 true만 해도 백버튼이 생김
+   //     actionBar.setHomeAsUpIndicator(R.drawable.back); //뒤로가기 버튼을 본인이 만든 아이콘으로 하기 위해 필요
+
+
+        bottomNavigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
 
         bottomNavigation.inflateMenu(R.menu.bottom_menu);
         fragmentManager = getSupportFragmentManager();
@@ -28,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 int id = item.getItemId();
-                switch (id){
+                switch (id) {
                     case R.id.action_bookshelf:
                         fragment = new com.example.hyemin.blinkling.BookshelfFragment();
                         break;
@@ -48,5 +62,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-}
 
+
+}
