dd72e642033157641bb7e86457a8f744b691a709
Dayeonii <swdy1233@gmail.com>
2017-08-06 02:10:05 +0900

    중간저장2

--- app/src/main/java/com/example/hyemin/blinkling/TextViewFragment.java
+++ app/src/main/java/com/example/hyemin/blinkling/TextViewFragment.java
@@ -3,7 +3,7 @@ package com.example.hyemin.blinkling;
 
 
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,6 +25,8 @@ public class TextViewFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+
         return inflater.inflate(R.layout.fragment_text_view, container, false);
     }
 
