944a950c624db248c58344e403824a0f316e33a2
서혜민 <seohyemin@seohyemin-ui-MacBook-Pro.local>
2017-09-11 15:08:22 +0900

    중간

--- app/src/main/java/com/example/hyemin/blinkling/BookShelf/BookshelfFragment.java
+++ app/src/main/java/com/example/hyemin/blinkling/BookShelf/BookshelfFragment.java
@@ -17,6 +17,9 @@ import com.example.hyemin.blinkling.R;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
+
+import static android.R.id.list;
 
 public class BookshelfFragment extends Fragment {
     File dir = Environment.getExternalStorageDirectory().getAbsoluteFile();
@@ -25,7 +28,10 @@ public class BookshelfFragment extends Fragment {
     final static GridViewAdapter gridadapter = new GridViewAdapter();
     String mBookName = "";
     String InStoragePath = Environment.getExternalStorageDirectory().getAbsolutePath() +"/Blinkling";
-  boolean init = true;
+    String mystoragepath = Environment.getExternalStorageDirectory().getAbsolutePath();
+
+    boolean init = true;
+    List mFileList;
 
     public BookshelfFragment() {
         // Required empty public constructor
@@ -36,11 +42,16 @@ public class BookshelfFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_bookshelf, container, false);
-        mFileGridView = (GridView) rootView.findViewById(android.R.id.list);
+        mFileGridView = (GridView) rootView.findViewById(list);
         mFileGridView.setAdapter(gridadapter);
-
+        this.mFileList = new ArrayList();
 
         if(init == true) {
+//            File[] Blinklist = getBlinklingList();
+//            showToBookShelf(Blinklist);
+//            init = false;
+
+
             String[] Blinklist = getBlinklingList();
             showToBookShelf(Blinklist);
             init = false;
@@ -66,11 +77,52 @@ public class BookshelfFragment extends Fragment {
         gridadapter.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.book1)
                 ,  mBookName_main) ;
 
-       gridadapter.notifyDataSetChanged();
+        gridadapter.notifyDataSetChanged();
         mBookName = mBookName_main;
 
     }
 
+//    String path="C:\";
+//    File dirFile=new File(path);
+//    File []fileList=dirFile.listFiles();
+//for(File tempFile : fileList) {
+//        if(tempFile.isFile()) {
+//            String tempPath=tempFile.getParent();
+//            String tempFileName=tempFile.getName();
+//            System.out.println("Path="+tempPath);
+//            System.out.println("FileName="+tempFileName);
+//            /*** Do something withd tempPath and temp FileName ^^; ***/
+//        }
+//    }
+
+//    public File[] getBlinklingList(){
+//       Activity root = getActivity();
+//        File fileRoot = new File(InStoragePath);
+//        if(fileRoot.isDirectory() == false){
+//            //파일이라면!!즉 처음 블링클링 안에 아무것도 없는 초기상태
+/////////////////////////////////////////////////////////////여기토스트수정///////////////////////////////////////////////////////////////////////////////////////
+//            Toast toast = Toast.makeText(root, "블링클링 폴더에 아무것도 없음", Toast.LENGTH_SHORT);
+//            toast.show();
+//            return null;
+//        }
+//
+//
+//        File[] stringFileList = fileRoot.listFiles();
+//
+//
+////        for( File ffile : stringFileList) {
+////            if(ffile.isFile()){
+////                mFileList.add( ffile.getName() );
+////            }
+////            else {
+////                Toast toast = Toast.makeText(root, "파일이 없음", Toast.LENGTH_SHORT);
+////                toast.show();
+////            }
+////        }
+//
+////            return mFileList;
+//        return stringFileList;
+//        }
 
 
     public String[] getBlinklingList(){
@@ -84,10 +136,26 @@ public class BookshelfFragment extends Fragment {
 //            return null;
 //        }
 
-            String[] stringFileList = fileRoot.list();
-            return stringFileList;
+        String[] stringFileList = fileRoot.list();
+        return stringFileList;
 
     }
+
+
+
+//    public void showToBookShelf(File[] stringFileList){
+//
+//        if(gridadapter.isEmpty() == true) {
+//            for (int i = 0; i < stringFileList.length; i++) {
+//                gridadapter.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.book1)
+//                        , stringFileList[i].getName());
+//            }
+//            gridadapter.notifyDataSetChanged();
+//        }
+//
+//
+//    }
+
 
 
     public void showToBookShelf(String []stringFileList){
@@ -105,7 +173,6 @@ public class BookshelfFragment extends Fragment {
 
 
 
-
     @Override
     public void onResume() {
         super.onResume();
@@ -116,7 +183,7 @@ public class BookshelfFragment extends Fragment {
 
 
     public void onPrepareOptionsMenu(Menu menu) {
-       menu.findItem(R.id.bookmark_btn).setVisible(false);
+        menu.findItem(R.id.bookmark_btn).setVisible(false);
         menu.findItem(R.id.voice_btn).setVisible(false);
         menu.findItem(R.id.eye_btn).setVisible(false);
         menu.findItem(R.id.light_btn).setVisible(false);
